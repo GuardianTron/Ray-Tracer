2a2f0ff99ee7d6d1d3f28f8a53e5c9df8e8c6b28
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-09-17 21:11:01 -0400

    Initial commit of Shape and Sphere classes.

---
+++ libraries/shapes.js
@@ -0,0 +1,85 @@
+ "use strict";
+
+import Vector3D from "./vector";
+
+ class Shape{
+
+    /**
+     * Calculates the parametric intersectons with the ray being cast.
+     * @param {Vector3D} originPoint 
+     * @param {Vector3D} rayVector 
+     * @returns Array
+     */
+    intersectsRayAt(originPoint,rayVector){
+        if(! (originPoint instanceof Vector3D)){
+            throw new TypeError("Origin Point must be instance of Vector3D");
+        }
+
+        if( ! (rayVector instanceof Vector3D)){
+            throw new TypeError("Ray Vector must be instance of Vector3D");
+        }
+        return [];
+    }
+ }
+
+ class Sphere extends Shape{
+
+    /**
+     * 
+     * @param {Vector3D} center 
+     * @param {Number} radius 
+     */
+
+    constructor(center,radius){
+        this.center = center;
+        this.radius = radius;
+    }
+
+    get center(){
+        return this._center;
+    }
+
+    set center(vector){
+        if(! (vector instanceof Vector3D)){
+            throw new TypeError("Center must be an instance of Vector3D");
+        }
+        this._center = vector;
+    }
+
+    get radius(){
+        return this._radius;
+    }
+
+    set radius(radius){
+        if(isNaN(radius)){
+            throw new TypeError("Radius must be of type Number");
+        }
+        this._radius = radius;
+    }
+
+    intersectsRayAt(originPoint,vector){
+        super.intersectsRayAt(originPoint,vector);
+        const directionVector = vector.subtract(originPoint);
+        const centerToOriginVector = this.center.subtract(originPoint);
+        //quadratic forumula constants for intersection.
+        const a = directionVector.dotProduct(directionVector);
+        const b = 2 * centerToOriginVector.dotProduct(directionVector);
+        const c = centerToOriginVector.dotProduct(centerToOrigin) - Math.pow(this.radius,2);
+
+        const disc = b*b - 4*a*c;
+
+        // no intersection
+        if(disc < 0){
+            return [Infinity,Infinity];
+        }
+
+        //intersections
+        const discSQRT = Math.sqrt(disc);
+        const t1 = (-b - discSQRT)/(2*a);
+        const t2 = (-b + discSQRT)/(2*a);
+        return [t1,t2];
+
+
+
+    }
+ }
