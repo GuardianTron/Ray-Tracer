bf5c6cb3fc13b798cf6417da79b503d6e0c1312a
Anthony Feeney <VigilanteNighthawk@gmail.com>
2022-11-22 03:03:26 -0500

    Specular demo now implements light cone when light is on correct side of surface.

--- js_demos/specular_demo.js
+++ js_demos/specular_demo.js
@@ -17,12 +17,45 @@ class SpecularDemoDraw extends DemoDrawBase{
     }
     
     drawLight = (x,y) =>{
-        const canvasDiagnal = (new Vector2D(this.canvas.width,this.canvas.height)).length;
+  
         const light = new Light(this.normal,this.surfaceRay,x,y)
         this._drawLineSegment(light.directionRaySegment,"blue");
         this._drawLineSegment(light.reflectedRaySegment,"purple");
+        //only draw arc if light is correct side of surface
+        if(light.directionRaySegment.unitVector.dotProduct(this.normal.unitVector) > 0) this._drawLightArc(light);
 
     }
+
+    _drawLightArc(light){
+        //calculate start and end angles of vectors relative to surface
+        const cameraAngle = Math.acos(this.cameraSegment.unitVector.cosineBetween(this.surfaceRay.direction));
+        const reflectedAngle = Math.acos(light.reflectedRaySegment.unitVector.cosineBetween(this.surfaceRay.direction));
+        const x = this.normal.start.x;
+        const y = this.normal.start.y;
+        const isCC = reflectedAngle > cameraAngle;
+        const cosine = light.reflectedRaySegment.vector.cosineBetween(this.cameraSegment.vector);
+        const intensity = Math.max(0,Math.floor(255*cosine));
+        const strokeColor = `rgb(${intensity},${intensity},0)`;
+        
+        const oldFillColor = this.ctx.fillStyle;
+        const oldStrokeColor = this.ctx.oldStrokeColor;
+        const oldLineWidth = this.ctx.lineWidth;
+        this.ctx.fillStyle = 'rgba(255,255,0,.25)';
+        this.ctx.strokeStyle = strokeColor;
+        this.ctx.lineWidth = 10;
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(x,y);
+        this.ctx.arc(x,y,this.normal.length,-1*cameraAngle,-1*reflectedAngle,isCC);
+        this.ctx.fill();
+        this.ctx.beginPath();
+        this.ctx.arc(x,y,this.normal.length,-1*cameraAngle,-1*reflectedAngle,isCC);
+        this.ctx.stroke();
+        this.ctx.fillStyle = oldFillColor;
+        this.ctx.strokeStyle = oldStrokeColor;
+        this.ctx.lineWidth = oldLineWidth;
+    }
+
 
 
 
@@ -47,5 +80,6 @@ class Light extends LightVector{
 
     }
 }
+
 
 runDemo(SpecularDemoDraw,'specular_demo_container');
